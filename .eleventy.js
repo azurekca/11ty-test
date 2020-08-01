fa50d9eaf041f99fa21b96c983a5eb2944ece4a4
Agata Zurek <agata.w.zurek@gmail.com>
2020-08-01 10:43:58 -0700

    remove jpg from setTemplateFormats

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,7 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.setTemplateFormats([
     "html",
     "liquid",
-    "yml",
-    "jpg"
+    "yml"
   ]);
 
   // copy static assets to output folder
