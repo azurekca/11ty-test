d12413e9316e1983d5ff12d34ee7c98b66db434b
Agata Zurek <agata.w.zurek@gmail.com>
2020-08-01 14:33:49 -0700

    moved map js into its own file

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = (eleventyConfig) => {
   
   // copy static assets to output folder
   eleventyConfig.addPassthroughCopy("./src/static/images");
+  eleventyConfig.addPassthroughCopy("./src/static/js")
 
   return {
     dir: {
